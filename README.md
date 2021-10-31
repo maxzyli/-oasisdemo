13b94e59d9c21e68120758e050e4bea183fc8437
maxzyli <maxzyli@me.com>
2021-10-31 12:22:24 +0800

    node_modules/@babel/core/lib/transformation/util/clone-deepJS

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # -oasisdemo
+
+
+
+
+npm init @oasis-engine/oasis-app
+yarn
+yarn create @oasis-engine/oasis-app
+After creating a template, run:
+npm install
+npm run dev
+
+
---
+++ src/oasis/index.ts
@@ -0,0 +1,86 @@
+import { OrbitControl } from "@oasis-engine/controls";
+import {
+  AssetType,
+  BackgroundMode,
+  Camera,
+  DiffuseMode,
+  DirectLight,
+  GLTFResource,
+  PrimitiveMesh,
+  SkyBoxMaterial,
+  SphericalHarmonics3,
+  TextureCubeMap,
+  WebGLEngine
+} from "oasis-engine";
+
+export function createOasis() {
+
+//-- create engine object
+const engine = new WebGLEngine("canvas");
+engine.canvas.resizeByClientSize();
+
+const scene = engine.sceneManager.activeScene;
+const { ambientLight, background } = scene;
+const rootEntity = scene.createRootEntity();
+
+const directLightNode = rootEntity.createChild("dir_light");
+const directLightNode2 = rootEntity.createChild("dir_light2");
+directLightNode.addComponent(DirectLight);
+directLightNode2.addComponent(DirectLight);
+directLightNode.transform.setRotation(30, 0, 0);
+directLightNode2.transform.setRotation(-30, 180, 0);
+
+//Create camera
+const cameraNode = rootEntity.createChild("camera_node");
+cameraNode.transform.setPosition(0, 0, 5);
+cameraNode.addComponent(Camera);
+cameraNode.addComponent(OrbitControl);
+
+// Create sky
+const sky = background.sky;
+const skyMaterial = new SkyBoxMaterial(engine);
+background.mode = BackgroundMode.Sky;
+sky.material = skyMaterial;
+sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
+
+Promise.all([
+  engine.resourceManager
+    .load<GLTFResource>("gltf/PBRHelmet.gltf")
+    .then((gltf) => {
+      rootEntity.addChild(gltf.defaultSceneRoot);
+      console.log(gltf);
+    }),
+  engine.resourceManager
+    .load<TextureCubeMap>({
+      urls: [
+        "/mdn/rms_7c464e/afts/img/A_4ebgQaWOLaIAAAAAAAAAAAAAARQnAQ.jpg",
+        "/mdn/rms_7c464e/afts/img/A_i56eR6AbreUAAAAAAAAAAAAAARQnAQ.jpg",
+        "/mdn/rms_7c464e/afts/img/A_3wYERKsel5oAAAAAAAAAAAAAARQnAQ.jpg",
+        "/mdn/rms_7c464e/afts/img/A_YiG7Srwmb3QAAAAAAAAAAAAAARQnAQ.jpg",
+        "/mdn/rms_7c464e/afts/img/A_VUUwQrAv47sAAAAAAAAAAAAAARQnAQ.jpg",
+        "/mdn/rms_7c464e/afts/img/A_Dn2qSoqzfwoAAAAAAAAAAAAAARQnAQ.jpg"
+      ],
+      type: AssetType.TextureCube
+    })
+    .then((cubeMap) => {
+      ambientLight.specularTexture = cubeMap;
+      skyMaterial.textureCubeMap = cubeMap;
+
+      ambientLight.diffuseMode = DiffuseMode.SphericalHarmonics;
+
+      const sh = new SphericalHarmonics3();
+      sh.setValueByArray([
+        0.2990323305130005, 0.46782827377319336, 0.6490488052368164, -0.08325951546430588, -0.1739923506975174,
+        -0.3481740653514862, 0.12110518664121628, 0.10342133790254593, 0.0647989809513092, 0.013654923066496849,
+        0.019375042989850044, 0.019014855846762657, -0.010647064074873924, -0.0158681683242321, -0.01735353097319603,
+        -0.06292672455310822, -0.06085652485489845, -0.04486454278230667, 0.19867956638336182, 0.21928717195987701,
+        0.19299709796905518, 0.01943504437804222, 0.03246982768177986, 0.04340629279613495, 0.13364768028259277,
+        0.19655625522136688, 0.21748234331607819
+      ]);
+      ambientLight.diffuseSphericalHarmonics = sh;
+    })
+]).then(() => {
+  engine.run();
+});
+}
+
